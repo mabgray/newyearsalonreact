f12fb7fbabbf39868ebc79111225d57bc193d872
Maryanne Gray <m2ry2nnegr2y@gmail.com>
2021-12-31 17:42:35 -0600

    react tutorial

---
+++ src/components/Location.js
@@ -0,0 +1,12 @@
+
+import React, { Component } from 'react';
+
+
+
+function App() {
+    return (
+       <div>Location Page</div>
+    )
+}
+
+export default Location;
---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Navbar() {
+    return (
+      <div>
+        <Link to="/">Home </Link>
+        <Link to="/about">About Us </Link>
+        <Link to="/Location">Location </Link>
+      </div>
+    );
+  };
